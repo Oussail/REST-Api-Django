7c7709e0c81a60f4e57e7c0bb8e722823cebe2d0
Oussail <oussail.alaoui@gmail.com>
2018-07-20 13:48:25 +0100

    create profile ViewSet

--- src/profiles_manager/profiles_api/views.py
+++ src/profiles_manager/profiles_api/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import viewsets
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
+from . import serializers
+from . import models
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    """Handles creating, reading and updating profiles."""
+
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
