d85edc981c4116416fe31e4233da22e4aa18e286
Oussail <oussail.alaoui@gmail.com>
2018-07-26 18:54:50 +0100

    Added improved permissions to use profile feed items API

--- src/profiles_manager/profiles_api/urls.py
+++ src/profiles_manager/profiles_api/urls.py
@@ -7,6 +7,7 @@ from . import views
 router = DefaultRouter()
 router.register('profile', views.UserProfileViewSet)
 router.register('login', views.LoginViewSet, base_name='login')
+router.register('feed', views.UserProfileFeedViewset)
 
 urlpatterns = [
     path('', include(router.urls)),
